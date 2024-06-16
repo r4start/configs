a5c55ab350e0be6af76bba685e55b030bc63876d
r4start <r4start@protonmail.ch>
2024-06-16 18:19:54 +0200

    Updated macos installation.

--- macos.sh
+++ macos.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env sh
 
+if ! command -v brew &> /dev/null; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 
-# Install brew
-# Install packages
+brew update
+brew upgrade
+
+brew install bat bottom dust eza go gopls grpcurl jq mosh neovim protobuf qemu ripgrep rust-analyzer rustup-init tig tmux
+brew install qbittorrent orbstack rectangle stats utm vlc
+brew install --cask alacritty --no-quarantine
+
+brew cleanup
+
 # Install oh-my-zsh
