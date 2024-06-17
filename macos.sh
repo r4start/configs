16b89e8f109c3adcfd318dbdef0b609b6ddcc380
Dmitry Sokolov <r4start@nebius.com>
2024-06-17 09:39:32 +0200

    Updated package list. Added mouse interactions in tmux.

--- macos.sh
+++ macos.sh
@@ -7,8 +7,8 @@ fi
 brew update
 brew upgrade
 
-brew install bat bottom dust eza go gopls grpcurl jq mosh neovim openvpn protobuf qemu ripgrep rust-analyzer rustup-init tig tmux
-brew install qbittorrent orbstack rectangle stats utm vlc logseq
+brew install bat bottom dust eza go gopls grpcurl jq mosh neovim openvpn protobuf qemu ripgrep rust-analyzer rustup-init tig tmux k9s yq virtctl terraform kind goose
+brew install qbittorrent orbstack rectangle stats utm vlc logseq macfuse sublime-text
 brew install --cask alacritty --no-quarantine
 
 brew cleanup
