2c960bdc7f58c4e0e23c424cc6c3e871fd876d32
Dmitry Sokolov <r4start@nebius.com>
2024-06-17 11:26:59 +0200

    Added few more packages.

--- macos.sh
+++ macos.sh
@@ -7,7 +7,10 @@ fi
 brew update
 brew upgrade
 
-brew install bat bottom dust eza go gopls grpcurl jq mosh neovim openvpn protobuf qemu ripgrep rust-analyzer rustup-init tig tmux k9s yq virtctl terraform kind goose
+brew install bat bottom dust eza go gopls grpcurl jq mosh neovim \
+             openvpn protobuf qemu ripgrep rust-analyzer rustup-init \
+             tig tmux k9s yq virtctl terraform kind goose python poetry \
+             helm
 brew install qbittorrent orbstack rectangle stats utm vlc logseq macfuse sublime-text
 brew install --cask alacritty --no-quarantine
 
