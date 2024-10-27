2983f31c9177f628f16aa7a2e1998e357061ece6
r4start <r4start@protonmail.ch>
2024-10-27 10:47:12 +0100

    Add karabiner-elements to Homebrew install list

--- macos.sh
+++ macos.sh
@@ -10,7 +10,7 @@ brew upgrade
 brew install bat bottom dust eza go gopls grpcurl jq mosh neovim \
              openvpn protobuf qemu ripgrep rust-analyzer rustup-init \
              tig tmux k9s yq virtctl terraform kind goose python poetry \
-             helm zsh-syntax-highlighting
+             helm zsh-syntax-highlighting karabiner-elements
 brew install qbittorrent orbstack rectangle stats utm vlc logseq macfuse sublime-text
 
 brew install --cask alacritty --no-quarantine
