6c2eb1eb77dfaf813d0f82c01a9f2efa15296323
r4start <r4start@protonmail.ch>
2025-01-07 17:01:56 +0100

    Updated fish config.

--- config.fish
+++ config.fish
@@ -8,5 +8,23 @@ export PATH="$HOME/.cargo/bin:$PATH"
 alias nbat="bat --paging=never"
 alias cat="bat --style=plain --paging=never"
 alias cls=clear
+
+# Kubernetes shorthands
 alias k=kubectl
+alias mk=minikube
+
 alias nv=nvim
+alias yy=paru
+
+# Git shorthands
+alias gst="git status"
+alias gcl="git clone"
+alias gbr="git branch"
+alias gco="git checkout"
+alias gc="git commit"
+alias gf="git fetch"
+alias gp="git push"
+alias gd="git diff"
+alias gl="git pull"
+
+set -g fish_greeting
