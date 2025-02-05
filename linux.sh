172904ee2646585a1ebbf94cf93500b4d02fc93c
Dmitry Sokolov <r4start@nebius.com>
2025-02-05 16:29:26 +0100

    Updated linux.sh

---
+++ linux.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+if ! command -v brew &> /dev/null; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+brew update
+brew upgrade
+
+brew install bat bottom dust eza fzf go gopls grpcurl jq mosh neovim \
+             openvpn protobuf ripgrep rust-analyzer rustup-init \
+             tig tmux k9s yq virtctl terraform kind goose python poetry \
+             helm yazi
+
+brew cleanup
