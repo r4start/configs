e931f608e419af9cbc2abc0fe91cc968038209ec
r4start <r4start@protonmail.ch>
2026-07-12 17:56:20 +0200

    Updated package list.

--- macos.sh
+++ macos.sh
@@ -7,14 +7,9 @@ fi
 brew update
 brew upgrade
 
-brew install --yes anki bat bottom dust eza fish fzf go gopls grpcurl jq neovim \
-             openvpn protobuf qemu ripgrep rust-analyzer rustup-init \
-             tig tmux k9s yq terraform minikube goose python poetry \
-             helm yazi just
+brew install --yes bat bottom dust eza fish fzf go gopls grpcurl jq neovim  protobuf qemu ripgrep rust-analyzer tig tmux k9s yq tfenv minikube helm yazi just
 brew install --yes coreutils findutils gnu-sed gawk gnu-which grep gnutls  gnu-indent gnu-getopt gnu-time make bash
-brew install --yes qbittorrent orbstack rectangle-pro stats utm vlc macfuse sublime-text
-
-brew install --yes --cask hiddenbar --no-quarantine
-brew install --yes --cask darktable codex commander-one zed
+brew install --yes orbstack rectangle-pro stats utm vlc
+brew install --yes --cask anki hiddenbar darktable codex commander-one zed transmission iterm2 font-fira-code-nerd-font firefox vivaldi libreoffice
 
 brew cleanup
